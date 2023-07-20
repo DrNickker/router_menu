144623e56fdeaa0c79e65862c63976217c39f64c
DrNickker <mintagirov2015@yandex.ru>
2023-07-20 13:23:37 +0500

    add_active

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export default function Menu() {
-    return (
-        <nav className="menu">
-            <NavLink className="menu__item" activeClassName="menu__item-active" exact to="/">Главная</NavLink>
-            <NavLink className="menu__item" activeClassName="menu__item-active" to="/drift">Дрифт-такси</NavLink>
-            <NavLink className="menu__item" activeClassName="menu__item-active" to="/timeattack">Time Attack</NavLink>
-            <NavLink className="menu__item" activeClassName="menu__item-active" to="/forza">Forza Karting</NavLink>
-        </nav>
-    )
+  return (
+    <nav className="menu">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "menu__item menu__item-active" : "menu__item"
+        }
+      >
+        Главная
+      </NavLink>
+      <NavLink
+        to="/drift"
+        className={({ isActive }) =>
+          isActive ? "menu__item menu__item-active" : "menu__item"
+        }
+      >
+        Дрифт-такси
+      </NavLink>
+      <NavLink
+        to="/timeattack"
+        className={({ isActive }) =>
+          isActive ? "menu__item menu__item-active" : "menu__item"
+        }
+      >
+        Time Attack
+      </NavLink>
+      <NavLink
+        to="/forza"
+        className={({ isActive }) =>
+          isActive ? "menu__item menu__item-active" : "menu__item"
+        }
+      >
+        Forza Karting
+      </NavLink>
+    </nav>
+  );
 }
