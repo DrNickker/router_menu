e194b648f7e20e390634d81cf1c2425552aa5bba
DrNickker <mintagirov2015@yandex.ru>
2023-07-20 13:19:55 +0500

    comp-elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Menu from './components/Menu'
-import HomePage from './components/HomePage'
-import DriftPage from './components/DriftPage'
-import ForzaPage from './components/ForzaPage'
-import TimeAttackPage from './components/TimeAttackPage'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TimeAttackPage from "./components/TimeAttackPage";
+import Menu from "./components/Menu";
+import HomePage from "./components/HomePage";
+import ForzaPage from "./components/ForzaPage";
+import DriftPage from "./components/DriftPage";
 
 export default function App() {
   return (
     <Router>
       <div>
         <Menu />
-          < div className="page">
+        <div className="page">
           <Routes>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
           </Routes>
         </div>
       </div>
