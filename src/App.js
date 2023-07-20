df49441746ba5e7de7a09462e5b125ec3f0cb101
DrNickker <mintagirov2015@yandex.ru>
2023-07-20 12:49:22 +0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Menu from './components/Menu'
 import HomePage from './components/HomePage'
 import DriftPage from './components/DriftPage'
@@ -12,11 +12,13 @@ export default function App() {
     <Router>
       <div>
         <Menu />
-        <div className="page">
+          < div className="page">
+          <Routes>
           <Route path="/" exact component={HomePage} />
           <Route path="/drift" component={DriftPage} />
           <Route path="/timeattack" component={TimeAttackPage} />
           <Route path="/forza" component={ForzaPage} />
+          </Routes>
         </div>
       </div>
     </Router>
